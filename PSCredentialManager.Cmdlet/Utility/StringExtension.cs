840e14e920db16a9c716dcaf1b7ae7116d9ca368
Dave Garnar <d.garnar@kelway.com>
2016-03-31 11:33:36 +0100

    adding method to convert secure string to string and wired up secure string parameter

--- PSCredentialManager.Cmdlet/Utility/StringExtension.cs
+++ PSCredentialManager.Cmdlet/Utility/StringExtension.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Security;
 using System.Text;
 
@@ -20,5 +21,20 @@ namespace PSCredentialManager.Utility
             return secureString;
         }
 
+        public static string ToInsecureString(this SecureString secureString)
+        {
+            IntPtr secureStringPtr = IntPtr.Zero;
+
+            try
+            {
+                secureStringPtr = Marshal.SecureStringToGlobalAllocUnicode(secureString);
+                return Marshal.PtrToStringUni(secureStringPtr);
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(secureStringPtr);
+            }
+
+        }
     }
 }
