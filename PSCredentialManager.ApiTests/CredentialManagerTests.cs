7d19bf24c688996cd2a867f3530575080c99af4a
Dave Garnar <d.garnar@kelway.com>
2016-04-22 10:12:59 +0100

    Starting to add unit test

---
+++ PSCredentialManager.ApiTests/CredentialManagerTests.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PSCredentialManager.Api;
+using PSCredentialManager.Api.Fakes;
+using PSCredentialManager.Common;
+using PSCredentialManager.Common.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PSCredentialManager.Api.Tests
+{
+    [TestClass()]
+    public class CredentialManagerTests
+    {
+        private static CredentialManager manager;
+        public CredentialManagerTests()
+        {
+            manager = new CredentialManager();
+        }
+
+        [TestMethod()]
+        public void WriteCredTest()
+        {
+            
+            using (ShimsContext.Create())
+            {
+                ShimImports.CredWriteNativeCredentialRefUInt32 =
+                   (ref NativeCredential credential, UInt32 flags) => 
+                {
+                    return true;
+                };
+
+                manager.WriteCred(new NativeCredential());
+            }
+        }
+
+        [TestMethod()]
+        public void ReadCredTest()
+        {
+            using (ShimsContext.Create())
+            {
+                ShimImports.CredReadStringCRED_TYPEInt32IntPtrOut =
+                    (string target, CRED_TYPE type, int flag, out IntPtr credentialPointer) =>
+                    {
+                        credentialPointer = new IntPtr();
+                        return true;
+                    };
+
+                ShimCriticalCredentialHandle.ConstructorIntPtr =
+                    (CriticalCredentialHandle credentialHandle, IntPtr credentialPointer) =>
+                    {
+
+                    };
+
+                ShimCriticalCredentialHandle.AllInstances.GetCredential =
+                    (CriticalCredentialHandle criticalCredentialHandle) =>
+                    {
+                        return new Credential();
+                    };
+
+                manager.ReadCred("server01", CRED_TYPE.GENERIC);
+            }
+        }
+
+        [TestMethod()]
+        public void DeleteCredTest()
+        {
+            using (ShimsContext.Create())
+            {
+                ShimImports.CredDeleteStringCRED_TYPEInt32 =
+                    (string target, CRED_TYPE type, int flag) =>
+                    {
+                        return true;
+                    };
+
+                manager.DeleteCred("server01", CRED_TYPE.GENERIC);
+            }
+        }
+
+        [TestMethod()]
+        public void ReadCredTest1()
+        {
+            using (ShimsContext.Create())
+            {
+                ShimImports.CredEnumerateStringInt32Int32OutIntPtrOut =
+                    (string filter, int flags, out int count, out IntPtr credentialPointer) =>
+                    {
+                        count = 1;
+                        credentialPointer = new IntPtr();
+                        return true;
+                    };
+
+                ShimCriticalCredentialHandle.ConstructorIntPtr =
+                    (CriticalCredentialHandle credentialHandle, IntPtr credentialPointer) =>
+                    {
+
+                    };
+
+                ShimCriticalCredentialHandle.AllInstances.GetCredentialsInt32 =
+                    (CriticalCredentialHandle credentialHandle, int count) =>
+                    {
+                        return new Credential[count];
+                    };
+
+                manager.ReadCred();
+            }
+        }
+    }
+}
---
+++ PSCredentialManager.ApiTests/Extensions/CredentialExtensionsTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PSCredentialManager.Api.Extensions;
+using PSCredentialManager.Common;
+using PSCredentialManager.Common.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace PSCredentialManager.Api.Extensions.Tests
+{
+    [TestClass()]
+    public class CredentialExtensionsTests
+    {
+        [TestMethod()]
+        public void ToNativeCredentialTest()
+        {
+            Credential credential = new Credential()
+            {
+                AttributeCount = 0,
+                Attributes = new IntPtr(0),
+                Comment = "This is a comment",
+                CredentialBlob = "April123!!",
+                CredentialBlobSize = 20,
+                Flags = 0,
+                LastWritten = new System.Runtime.InteropServices.ComTypes.FILETIME(),
+                Persist = CRED_PERSIST.LOCAL_MACHINE,
+                TargetName = "server01",
+                Type = CRED_TYPE.GENERIC,
+                UserName = "test-user"
+            };
+
+            NativeCredential nativeCredential = credential.ToNativeCredential();
+
+            Assert.IsNotNull(nativeCredential);
+            Assert.IsInstanceOfType(nativeCredential, typeof(NativeCredential));
+        }
+    }
+}
